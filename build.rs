0f6b436f4f179f702aaca3ac76edf29c859eae09
Ghin_MLo7 <mashini.ruroii@gmail.com>
2026-02-15 00:35:45 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("jiafei.ico");
-    res.compile().unwrap();
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("jiafei.ico");
+        res.compile().unwrap();
+    }
 }
